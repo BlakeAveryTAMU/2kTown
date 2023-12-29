7ba65394b6168d632181523c8c046af045cb7876
BlakeAveryTAMU <blakeavery42@tamu.edu>
2023-12-28 20:19:21 -0600

    Added error popup for missing input fields during login and signup

--- Assets/Scripts/FirebaseController.cs
+++ Assets/Scripts/FirebaseController.cs
@@ -5,30 +5,48 @@ using MyNavigator;
 using UnityEngine.UI;
 using System;
 using TMPro;
+using UnityEngine.Tilemaps;
 
 public class FirebaseController : MonoBehaviour
 {
 
     public TMP_InputField loginEmail, loginPassword, signupEmail, signupPassword, signupConfirmPassword;
 
+    public GameObject errorPopup;
+    public TMP_Text errorTitle, errorMessage;
+
     public void LoginUser(){
 
         // check if info is empty
         if (string.IsNullOrEmpty(loginEmail.text) || string.IsNullOrEmpty(loginPassword.text)){
-            Console.WriteLine("Empty Username or Password");
+            showErrorPopup("Error", "Missing Email or Password");
             return;
         }
 
-        // Log in the user
+        // Log in the user and display pop up if sign in successful or unsuccessful
     }
 
     public void SignupUser(){
 
         if (string.IsNullOrEmpty(signupEmail.text) || string.IsNullOrEmpty(signupConfirmPassword.text) || string.IsNullOrEmpty(signupPassword.text)){
-            Console.WriteLine("Empty Username or Password");
+            showErrorPopup("Error", "Missing Email or Password");
             return;
         }
 
-        // Do sign up
+        // Do sign up, then have a pop up that redirects to login
+    }
+
+    private void showErrorPopup(string title, string message){
+
+        errorTitle.text = "" + title;
+        errorMessage.text = "" + message;
+
+        errorPopup.SetActive(true);
+    }
+
+    public void closeErrorPopup(){
+        errorTitle.text = "";
+        errorMessage.text = "";
+        errorPopup.SetActive(false);
     }
 }
